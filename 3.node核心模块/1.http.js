8b2f515c26ddf4680507fb9be94cff116b5f0650
123456hawkins <2398430768@qq.com>
2023-12-19 18:04:31 +0800

    新增http

--- 3.node核心模块/1.http.js
+++ 3.node核心模块/1.http.js
@@ -1,7 +1,20 @@
 const http = require('http')
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('Hello World!')
+const server=http.createServer((req,res)=>{
+  // 这两个变量也是 stream 类型，前者是readable stream(可读流)，后者是writeable stream(可写流)，
+  // 从字面意思上推测出前者是用来读取数据的，而后者是用来写入数据的
+
+  res.writeHead(200,{
+    'content-type':'text/plain'
   })
-  .listen(8081)
+  res.end('hello Node JS!')
+})
+server.listen(3000,()=>{
+  console.log('listen on port 3000');
+})
+
+setTimeout(() => {
+  console.error(new Error('Whoops, something bad happened'));
+  server.close(()=>{
+    console.log('server close！');
+  })
+}, 3000);
